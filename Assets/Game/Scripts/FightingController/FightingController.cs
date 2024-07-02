2bce84cea3dbcc60dcd50aa2e7dfa7ee5d7986b3
utsavagarwal28 <utsavagarwal2002@gmail.com>
2024-07-02 12:27:40 +0530

    Player Attack and Dodge System

--- Assets/Game/Scripts/FightingController/FightingController.cs
+++ Assets/Game/Scripts/FightingController/FightingController.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class FightingController : MonoBehaviour
 {
-                            
+    [Header("Player Movement")]
     public float movementSpeed = 3f;
     public float rotationSpeed = 10f;
     private CharacterController characterController;
     private Animator animator;
+
+    [Header("Player Fight")]
+    public float attackCooldown = 0.5f;
+    public int attackDamages = 5;
+    public string[] attackAnimations = { "Attack1Animation", "Attack2Animation", "Attack3Animation", "Attack4Animation" };
+    public float dodgeDistance = 2f;
+    private float lastAttackTIme;
 
     void Awake()
     {
@@ -19,40 +26,93 @@ public class FightingController : MonoBehaviour
     void Update()
     {
         PerformMovement();
-    }
+        PerformDodgeFront();
 
-    void PerformMovement()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(-verticalInput, 0, horizontalInput);
-
-        if (movement != Vector3.zero)
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
+            PerformAttack(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PerformAttack(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            PerformAttack(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            PerformAttack(3);
+        }
 
-            Quaternion targetRotation = Quaternion.LookRotation(movement);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
-            if (horizontalInput > 0)
+        void PerformMovement()
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(-verticalInput, 0, horizontalInput);
+
+            if (movement != Vector3.zero)
             {
-                animator.SetBool("Walking", true);
-            }
-            else if (horizontalInput < 0)
-            {
-                animator.SetBool("Walking", true);
+
+                Quaternion targetRotation = Quaternion.LookRotation(movement);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+                if (horizontalInput > 0)
+                {
+                    animator.SetBool("Walking", true);
+                }
+                else if (horizontalInput < 0)
+                {
+                    animator.SetBool("Walking", true);
+                }
+                else
+                {
+                    animator.SetBool("Walking", true);
+                }
             }
             else
             {
-                animator.SetBool("Walking", true);
+                animator.SetBool("Walking", false);
             }
-        }
-        else
-        {
-            animator.SetBool("Walking", false);
+
+            characterController.Move(movement * movementSpeed * Time.deltaTime);
+
         }
 
-        characterController.Move(movement * movementSpeed * Time.deltaTime);
+        void PerformAttack(int attackIndex)
+        {
+            if (Time.time - lastAttackTIme > attackCooldown)
+            {
+                animator.Play(attackAnimations[attackIndex]);
+
+                int damage = attackDamages;
+                Debug.Log("Performed attack" + (attackIndex + 1) + "dealing" + damage + "damage.");
+
+                lastAttackTIme = Time.time;
+
+                // Loop through each opponent.
+            }
+            else
+            {
+                // If the player tries to attack too quickly, inform them:
+                Debug.Log("Cannot perform attack yet. Cooldown time left");
+            }
+        }
+
+        void PerformDodgeFront()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                animator.Play("DodgeFrontAnimation");
+
+                Vector3 dodgeDirection = transform.forward * dodgeDistance;
+
+                characterController.Move(dodgeDirection);
+
+            }
+        }
 
     }
 }
