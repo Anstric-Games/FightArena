630e3bcef7d6ddfff8d63750dc84ccc8184f198d
utsavagarwal28 <utsavagarwal2002@gmail.com>
2024-07-07 17:28:35 +0530

    Result UI

---
+++ Assets/Game/Scripts/UI/ResultManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ResultManager : MonoBehaviour
+{
+    public GameObject resultPanel;
+    public Text resultText;
+
+    public FightingController[] fightingController;
+    public OpponentAI[] opponentAI;
+
+    private void Update()
+    {
+        foreach (OpponentAI opponentAI in opponentAI)
+        {
+            if (opponentAI.gameObject.activeSelf && opponentAI.currentHealth <= 0)
+            {
+                SetResult("You Win");
+                return;
+            }
+        }
+    }
+
+    void SetResult(string result)
+    {
+        resultText.text = result;
+        resultPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
